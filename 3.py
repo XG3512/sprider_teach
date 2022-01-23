ab0a1486e962d8e163c1eea04af43a90137cdca8
xg <xg>
2022-01-23 17:26:44 +0800

    1

--- 3.py
+++ 3.py
@@ -3,12 +3,15 @@ from bs4 import BeautifulSoup
 header={
 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36"
 }
+
 url = "https://ssr1.scrape.center/page/1"
 
 init_page = requests.get(url,headers = header).content
 init_soup = BeautifulSoup(init_page, 'html.parser')
 
-all_movies = init_soup.find('div', class_="el-col el-col-18 el-col-offset-3")
+all_movies = init_soup.find('div', class_="el-col.el-col-18.el-col-offset-3")
+print(all_movies)
+print(type(all_movies))
 for each_movie in all_movies.find_all('div', class_="el-card"):
     all_a_tag = each_movie.find_all('h2')
 
