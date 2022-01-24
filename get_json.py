9bc80357ba90d82bdce1cb1781d66a6e3ab7e462
xg <2412650616@qq.com>
2022-01-24 21:43:38 +0800

    Add getdata function and add a loop to traverse the entire link

--- get_json.py
+++ get_json.py
@@ -1,8 +1,9 @@
 import requests
-import json
-url = "https://spa3.scrape.center/api/movie/?limit=10&offset=40"
-r = requests.get(url)
-
-print(r.json()['results'][0]['categories'])
-for i  in r.json()['results'][0]['categories']:
-    print(i)
+def getdata(url):
+    r = requests.get(url)
+    for i  in r.json()['results']:
+        print(i['name'])
+for j in range(10,101,10):
+    url="https://spa3.scrape.center/api/movie/?limit=10&offset="+str(j)
+    print(url)
+    getdata(url)
